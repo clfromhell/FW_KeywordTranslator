ff04f803d32b825d865e8f88ba37c11dc4c8544f
Stefan Hackermeier <hackermeier@gmail.com>
2018-03-05 17:07:50 +0100

    Fixes and added comments

--- main.py
+++ main.py
@@ -6,26 +6,34 @@ from shutil import copyfile
 
 cp = ConfigParser.SafeConfigParser()
 
+# Get current directory and set file paths
 loc_dir = os.path.dirname(os.path.realpath(__file__))
 cfg = loc_dir + "\\Settings.cfg"
 Dictionary = loc_dir + "\\Stichw.csv"
 
+# Read Settings.cfg
 cp.read(cfg)
 
+# Set timestamp for outfile
 ts = datetime.strftime(datetime.today(),"%d-%m-%Y--%H-%M-%S")
 
+# Check if Temp-Folder exists and if not, create the folder and exit (If there's no folder, there's also no file to be checked :-) )
 if not os.path.isdir(loc_dir + "\\Temp"):
 	os.makedirs(loc_dir + "\\Temp")
+	sys.exit(0)
 
 try:	
+	
+	# Walk through the Temp-Folder and get the filename
 	for root, dirs, files in os.walk(loc_dir + "\\Temp"):
 		for file in files:
 			f = file
-
+	
+	# Open the file and read it
 	fax = open(loc_dir + "\\Temp\\" + str(f), 'r')
-
 	t = fax.read()
-
+	
+	# Loop through the Keyword Dictionary and replace Values
 	with open(Dictionary, 'r') as d:
 		next(d)
 		for line in d:
@@ -36,7 +44,8 @@ try:
 				t = t.replace(ov, nv)
 				
 			del arrline[:]
-
+	
+	# Create the Destination-File in the Folder specified in Settings.cfg
 	nf = open(str(cp.get("Allgemein", "Ziel")) + "\\" + ts + ".txt", 'w')
 	nf.write(t)
 	nf.close()
@@ -44,6 +53,8 @@ try:
 	fax.close()
 	
 except:
+	
+	# If something's wrong, just copy the original File to the Destination Folder
 	try:
 		copyfile(t, str(cp.get("Allgemein", "Ziel")) + "\\" + ts + ".txt")
 	except:
@@ -52,9 +63,11 @@ except:
 	pass
 
 try:
+	
+	# Delete the original File if possible
 	os.remove(root + "\\" + f)
 except:
 	pass
 	
-sys.exit(0)
+#sys.exit(0)
 
