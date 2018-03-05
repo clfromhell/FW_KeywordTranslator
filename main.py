2dc7883c3e53f431c86bc349e9362f53251ae48a
Stefan Hackermeier <hackermeier@gmail.com>
2018-03-05 17:25:31 +0100

    Added compatibility for Linux + pip requirements

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from datetime import datetime
 from shutil import copyfile
 import platform
 
-# Check if Windows (required!) is used
-if not platform.system() == 'Windows':
-	print "Wrong Operating System. Exiting."
-	sys.exit(0)
+# Workaround for compatibility issues with different operating systems
+if platform.system() == 'Linux':
+	dirvar = "/"
+elif platform.system() == 'Windows':
+	dirvar = "\\"
 
 cp = ConfigParser.SafeConfigParser()
 
 # Get current directory and set file paths
 loc_dir = os.path.dirname(os.path.realpath(__file__))
-cfg = loc_dir + "\\Settings.cfg"
-Dictionary = loc_dir + "\\Stichw.csv"
+cfg = loc_dir + dirvar + "Settings.cfg"
+Dictionary = loc_dir + dirvar + "Stichw.csv"
 
 # Read Settings.cfg
 cp.read(cfg)
@@ -24,19 +25,19 @@ cp.read(cfg)
 ts = datetime.strftime(datetime.today(),"%d-%m-%Y--%H-%M-%S")
 
 # Check if Temp-Folder exists and if not, create the folder and exit (If there's no folder, there's also no file to be checked :-) )
-if not os.path.isdir(loc_dir + "\\Temp"):
-	os.makedirs(loc_dir + "\\Temp")
+if not os.path.isdir(loc_dir + dirvar + "Temp"):
+	os.makedirs(loc_dir + dirvar + "Temp")
 	sys.exit(0)
 
 try:	
 	
 	# Walk through the Temp-Folder and get the filename
-	for root, dirs, files in os.walk(loc_dir + "\\Temp"):
+	for root, dirs, files in os.walk(loc_dir + dirvar + "Temp"):
 		for file in files:
 			f = file
 	
 	# Open the file and read it
-	fax = open(loc_dir + "\\Temp\\" + str(f), 'r')
+	fax = open(loc_dir + dirvar + "Temp" + dirvar + str(f), 'r')
 	t = fax.read()
 	
 	# Loop through the Keyword Dictionary and replace Values
@@ -52,7 +53,7 @@ try:
 			del arrline[:]
 	
 	# Create the Destination-File in the Folder specified in Settings.cfg
-	nf = open(str(cp.get("Allgemein", "Ziel")) + "\\" + ts + ".txt", 'w')
+	nf = open(str(cp.get("Allgemein", "Ziel")) + divar + ts + ".txt", 'w')
 	nf.write(t)
 	nf.close()
 	
@@ -62,7 +63,7 @@ except:
 	
 	# If something's wrong, just copy the original File to the Destination Folder
 	try:
-		copyfile(t, str(cp.get("Allgemein", "Ziel")) + "\\" + ts + ".txt")
+		copyfile(t, str(cp.get("Allgemein", "Ziel")) + dirvar + ts + ".txt")
 	except:
 		pass
 	
@@ -71,7 +72,7 @@ except:
 try:
 	
 	# Delete the original File if possible
-	os.remove(root + "\\" + f)
+	os.remove(root + dirvar + f)
 except:
 	pass
 	
